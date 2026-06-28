86170cbf9d67c9ba75e57bb7dac5318d795939ca
RITOMCYT <163521003+RITOMCYT@users.noreply.github.com>
2026-06-28 12:15:26 +0700

    Upgrade Material Components to version 1.12.0
    Updated Material Components library version to 1.12.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.material:material:1.11.0") 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
